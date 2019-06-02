4d96137aba5e8a511edb02babb89b5ffac34b053
vanness30214 <vanness@vip.qq.com>
2019-06-02 23:28:21 +0800

    index,register,login

--- components/index/artistic.vue
+++ components/index/artistic.vue
@@ -1,25 +1,44 @@
 <template>
   <section class="m-istyle">
     <dl >
-      <dt>有格调</dt>
-      <dd>全部</dd>
-      <dd>约会聚餐</dd>
-      <dd>丽人SPA</dd>
-      <dd>电影演出</dd>
-      <dd>品质出游</dd>
+      <dd
+        :class="{active:kind==='all'}"
+        kind="all"
+        keyword="景点">全部</dd>
+      <dd
+        :class="{active:kind==='part'}"
+        kind="part"
+        keyword="美食">约会聚餐</dd>
+      <dd
+        :class="{active:kind==='spa'}"
+        kind="spa"
+        keyword="丽人">丽人SPA</dd>
+      <dd
+        :class="{active:kind==='movie'}"
+        kind="movie"
+        keyword="电影">电影演出</dd>
+      <dd
+        :class="{active:kind==='travel'}"
+        kind="travel"
+        keyword="旅游">品质出游</dd>
     </dl>
     <ul class="ibody">
-      <li>
-        <el-card>
-          <img
-            class="image">
-          <ul class="cbody">
-            <li class="title">大东</li>
-            <li class="pos"><span>建议2人实用</span></li>
-            <li class="price"><em>15</em><span>/起</span></li>
-          </ul>
-        </el-card>
-      </li>
+      <li
+      v-for="item in cur"
+      :key="item.title">
+      <el-card
+        :body-style="{ padding: '0px' }"
+        shadow="never">
+        <img
+          :src="item.img"
+          class="image">
+        <ul class="cbody">
+          <li class="title">{{ item.title }}</li>
+          <li class="pos"><span>{{ item.pos }}</span></li>
+          <li class="price">￥<em>{{ item.price }}</em><span>/起</span></li>
+        </ul>
+      </el-card>
+    </li>
     </ul>
   </section>
 </template>
@@ -28,9 +47,20 @@ export default {
   name:'artistic',
   data(){
     return{
-
+      kind: 'all',
+      list: {
+        all: [],
+        part: [],
+        spa: [],
+        movie: [],
+        travel: []
+      }
     }
-  }
+  },computed: {
+    cur: function () {
+      return this.list[this.kind]
+    }
+  },
 
 }
 </script>
---
+++ server/interface/users.js
@@ -0,0 +1,3 @@
+import Router from 'koa-router';
+import Redis from 'koa-redis';
+import nodeMailer from 'nodemailer';
